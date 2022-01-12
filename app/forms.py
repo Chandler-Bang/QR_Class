50dc0d4f31f9e431344852d4cc3897e8107d43e9
Chandler <bush8838@gmail.com>
2022-01-12 17:28:00 +0800

    Add subject and chapter pages, and build database

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,25 @@
+from flask.helpers import flash
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField, SelectField, FloatField
 from wtforms import DateTimeField, IntegerField, StringField
-from wtforms.validators import DataRequired
-from datetime import datetime
+from wtforms.validators import DataRequired, Length
+from datetime import date
+
+
+class AddSubjectForm(FlaskForm):
+    subject = StringField(label="学科名称",
+                          validators=[DataRequired(), Length(1, 20)])
+    submit = SubmitField(label="提交")
+
+
+class AddChapterForm(FlaskForm):
+    chapter = StringField(label="章节",
+                          validators=[DataRequired(), Length(1, 20)])
+    submit = SubmitField(label="提交")
 
 
 class AddQuestionForm(FlaskForm):
-    chapter = StringField(label='章节')  # 有待完善
+    chapter = SelectField(label="章节选择")  # 有待完善
     questionText = StringField(label='题目正文', validators=[DataRequired()])
     choice1 = StringField(label='A')
     choice2 = StringField(label='B')
@@ -14,6 +27,6 @@ class AddQuestionForm(FlaskForm):
     choice4 = StringField(label='D')
     answer = StringField(label='答案')
     difficulity = FloatField(label='难度选择')
-    addTime = DateTimeField(label='添加时间', default=datetime.utcnow())
+    addTime = DateTimeField(label='添加时间', default=date.today())
     addPerson = StringField(label='添加人')
     submit = SubmitField(label='提交')
