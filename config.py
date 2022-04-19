6dae893240035c47ca2e98b10d95581c87da639a
patrick <patrick@server.com>
2022-04-19 21:36:13 +0800

    production branch

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import os
+import sys
 
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'guess what'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'mysql+pymysql://root:chinaren@localhost:3306/QRclass'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+'mysql+pymysql://root:chinaren110@localhost:3306/QRclass'
+
+
+config = {
+    'production': Config
+}
